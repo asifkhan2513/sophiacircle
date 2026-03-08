51e4b0249d6a997126be2bfbe7a7c5bc79b32dff
Mo Asif <141583278+asifkhan2513@users.noreply.github.com>
2026-03-09 01:34:11 +0530

    port update

--- server/index.ts
+++ server/index.ts
@@ -19,7 +19,8 @@ connectDB();
 cloudinaryConnect();
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = Number(process.env.PORT) || 5000;
+const HOST = "0.0.0.0";
 
 // middlewares
 app.use(
@@ -63,8 +64,8 @@ app.use(
   }
 );
 
-app.listen(PORT, () => {
-  console.log(` Server running on http://localhost:${PORT}`);
+app.listen(PORT, HOST, () => {
+  console.log(` Server running on http://${HOST}:${PORT}`);
 });
 
 export default app;
